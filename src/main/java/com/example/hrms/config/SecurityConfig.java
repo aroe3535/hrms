58ac0e8bfe9c453b2d418fc055ddf534a7938683
Shion <shion.nakamura0305@gmail.com>
2026-02-10 04:40:30 +0900

    configにsecurity追加

---
+++ src/main/java/com/example/hrms/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.hrms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()
+            )
+            .csrf(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
