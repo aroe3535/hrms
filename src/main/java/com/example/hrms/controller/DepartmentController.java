44d37834021b8d3136cfec454e295c6af24e03b0
Shion <shion.nakamura0305@gmail.com>
2026-02-09 13:55:55 +0900

    新規登録画面の追加

--- src/main/java/com/example/hrms/controller/DepartmentController.java
+++ src/main/java/com/example/hrms/controller/DepartmentController.java
@@ -5,15 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.hrms.entity.Department;
+import com.example.hrms.repository.DepartmentRepository;
 import com.example.hrms.service.DepartmentService;
 
 @Controller
 public class DepartmentController {
 
+    private final DepartmentRepository departmentRepository;
+
     private final DepartmentService departmentService;
 
-    public DepartmentController(DepartmentService departmentService) {
+    public DepartmentController(DepartmentService departmentService, DepartmentRepository departmentRepository) {
         this.departmentService = departmentService;
+        this.departmentRepository = departmentRepository;
     }
 
     @GetMapping("/departments")
@@ -26,6 +31,22 @@ public class DepartmentController {
 public String delete(@PathVariable Long id, RedirectAttributes ra) {
     departmentService.delete(id);
     ra.addFlashAttribute("message", "部署情報を削除しました");
+    return "redirect:/departments";
+}
+
+@GetMapping("/departments/new")
+public String newDepartment() {
+    return "departments/new";
+}
+
+@PostMapping("/departments/create")
+public String createDepartment(
+        @RequestParam String departmentName) {
+
+    Department dept = new Department();
+    dept.setDepartmentName(departmentName);
+    departmentRepository.save(dept);
+
     return "redirect:/departments";
 }
 
